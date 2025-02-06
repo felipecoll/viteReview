e6f25490712870a7e66b6abaad9c8648323c4f14
Felipe Coll <leonfelipecoll@gmail.com>
2025-02-06 10:57:29 -0300

    Fin configuracion archivo vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { resolve } from 'path'
 
-export default defineConfig(() => {
+export default defineConfig(({ command, mode }) => {
   const port = 3000
 
-  return {
-    server: {
-      port
+  console.log(command, mode)
+  const env = loadEnv(mode, process.cwd())
+
+  console.log(env.VITE_NAME)
+
+  if (mode === "development") {
+    console.log("modo desarrollo")
+    return {
+      server: {
+        port
+      }
+    }
+  } else {
+    console.log("modo produccion")
+    return {
+      build: {
+        lib: {
+          entry: resolve(__dirname, 'lib', 'main.js'),
+          name: 'demo',
+          fileName: (format) => `demo.${format}.js`
+        }
+      }
     }
   }
 })
